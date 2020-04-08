3c738d35c84af49fd32f16e3396276c201765b9d
nvhoang191 <nvhoang191@gmail.com>
2020-04-08 15:59:10 +0700

    feat: update Lab03 Tweening

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -13,14 +13,14 @@
 #endif
 
 /* 	
-	P = (1-t)*A + t*B => P is one of the points when tweening from A to B
-	t = 0 => just A, t = 1 => just B
-	P (0 <= t <= 1)	
+	Theory:
+		P = (1-t)*A + t*B => P is the point calculated at time t while mapping from A to B
+		P (0 <= t <= 1), t = 0 => that's A, t = 1 => that's B
 */
 
 // screen constants
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 700;
 
 struct Point {
 	float x, y;
@@ -31,33 +31,42 @@ struct Point {
 };
 
 Point *startPoints, *endPoints;
-float deltaT = 0.0005;
+float deltaT = 0.0002;
 float currentT = 0.0;
 int numberOfPoints;
 
-
 void initBase(void) {
-  	glClearColor(0.0, 0.0, 0.0, 0.0); // white background
-  	//glMatrixMode(GL_PROJECTION);
+  	glClearColor(1.0, 1.0, 1.0, 0.0); // white background	
   	glLoadIdentity();
   	glOrtho(0.0, SCREEN_WIDTH, 0.0, SCREEN_HEIGHT, -1.0, 1.0); // left, right, bottom, top, near, and far values in the arguments
 }
 
-// initialize 
+// initialize points and undertake point mapping
 void initPoints(){
-	// as well as the number of vertex pairs
-	numberOfPoints = 3;
+	numberOfPoints = 9; // as well as the number of vertex pairs
 	
 	startPoints = new Point[numberOfPoints];
 	endPoints = new Point[numberOfPoints];
 	
 	startPoints[0] = Point(100.f, 100.f);
-	startPoints[1] = Point(200.f, 300.f);
-	startPoints[2] = Point(100.f, 500.f);
-	
-	endPoints[0] = Point(700.f, 500.f);
-	endPoints[1] = Point(600.f, 300.f);
-	endPoints[2] = Point(700.f, 100.f);
+	startPoints[1] = Point(100.f, 400.f);
+	startPoints[2] = Point(150.f, 430.f);
+	startPoints[3] = Point(200.f, 460.f);
+	startPoints[4] = Point(250.f, 490.f);
+	startPoints[5] = Point(300.f, 460.f);
+	startPoints[6] = Point(350.f, 430.f);
+	startPoints[7] = Point(400.f, 400.f);
+	startPoints[8] = Point(400.f, 100.f);
+		
+	endPoints[0] = Point(700.f, 200.f);
+	endPoints[1] = Point(700.f, 300.f);
+	endPoints[2] = Point(800.f, 300.f);
+	endPoints[3] = Point(830.f, 400.f);
+	endPoints[4] = Point(900.f, 400.f);
+	endPoints[5] = Point(970.f, 400.f);
+	endPoints[6] = Point(1000.f, 300.f);
+	endPoints[7] = Point(1100.f, 300.f);
+	endPoints[8] = Point(1100.f, 200.f);
 }
 
 // lerp function
@@ -65,10 +74,11 @@ float lerp(float A, float B, float t){
 	return (1-t)*A + t*B;
 }
 
-// tweening, use GL_LINE_LOOP to connect every single point in turn together (1->2->3->...)
+// tweening, use GL_LINE_LOOP to connect every single point in turn (1->2->3->...)
 void drawTweening(Point *start, Point *end, int n, float t){
 	glBegin(GL_LINE_LOOP);
 	for (int i=0; i<n; i++){
+		glColor3f(0.0, 0.3, 0.4);
 		// calculate x, y at time t
 		float x = lerp(start[i].x, end[i].x, t);
 		float y = lerp(start[i].y, end[i].y, t);
@@ -89,22 +99,18 @@ void display(void){
 
 /* ----------------------------------------------------------------------- */
 
-int main( int argc, char *argv[] )  {
-  	// Initialize GLUT.
+int main(int argc, char *argv[]) {
   	glutInit(&argc, argv);
-  	// Set the mode to draw in.
   	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  	// Set the window size in screen pixels.
   	glutInitWindowSize(1, 1);
-  	// Set the window position in screen pixels.
   	glutInitWindowPosition(100, 100);
-  	// Create the window.
-  	glutCreateWindow ("NguyenVanHoang_16020231_Tuan3");
+  	
+  	glutCreateWindow ("NguyenVanHoang_16020231_Tuan03");
   	glutReshapeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
   	
   	initBase();
   	initPoints();
-  	// Set rendering function
+  	
   	glutDisplayFunc( display );
   	glutMainLoop( );
   	return 0;
